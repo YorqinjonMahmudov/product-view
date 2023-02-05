c2c6536caa4c6325dd7d2b0a0e6d5e209f3c2904
yorqinjon <yorqinjonmahmudov93@gmail.com>
2023-02-05 23:09:53 +0500

    creating child component and passing props

--- src/views/ProductOneView.vue
+++ src/views/ProductOneView.vue
@@ -1,21 +1,24 @@
 <template>
-<h1>{{ product}}</h1>
-<button @click="deleteFromChild"> delete</button>
+  <div class="child">
+    <button @click="deleteFromChild"> delete from child component</button>
+    <h3> being deleted name: {{ product.name_uz }}</h3>
+    <h3> being deleted id: {{ product.id }}</h3>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'ProductOne',
-  props:{
-    product:{}
+  props: {
+    product: {}
 
   },
   data: function () {
 
   },
   methods: {
-    deleteFromChild(){
+    deleteFromChild() {
       this.$parent.openDeleteModal(this.product)
     }
 
@@ -23,3 +26,20 @@ export default {
 
 }
 </script>
+
+<style>
+
+.child button {
+  padding: 10px 20px;
+  margin-left: 20px;
+  height: 50px;
+  cursor: pointer;
+  width: 60%;
+  background-color: red;
+}
+
+.child h3 {
+  margin-left: 20px;
+}
+
+</style>
